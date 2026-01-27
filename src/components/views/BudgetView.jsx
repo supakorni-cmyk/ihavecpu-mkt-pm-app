86812a0547242292e826c578170c689f1649a581
supakorni-cmyk <supakorn.i@ihavecpu.com>
2026-01-27 15:37:36 +0700

    Add More Filtered Income

--- src/components/views/BudgetView.jsx
+++ src/components/views/BudgetView.jsx
@@ -1,5 +1,5 @@
 // src/components/views/BudgetView.jsx
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { 
   TrendingUp, 
   TrendingDown, 
