5fc31ff60a9d9b3f2f68380a4e5853fbfc298993
supakorni-cmyk <supakorn.i@ihavecpu.com>
2026-02-19 11:41:48 +0700

    Responsive

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -76,7 +76,7 @@ export default function Dashboard() {
   }, [data.tasks]);
 
   return (
-    <div className="flex h-screen w-full bg-gray-50 font-sans overflow-hidden">
+  <div className="flex flex-col md:flex-row h-screen w-full bg-gray-50 font-sans overflow-hidden">
       
       <Sidebar 
         currentView={currentView} 
