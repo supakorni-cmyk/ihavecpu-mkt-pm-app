b6edf60cc22e2ae3378924365e96d3131be9860e
supakorni-cmyk <supakorn.i@ihavecpu.com>
2026-06-16 11:42:41 +0700

    Social Summarizer

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -22,7 +22,7 @@ import SelfHealView from './components/views/SelfHealView';
 import ReportView from './components/views/ReportView';
 import OTView from './components/views/OtView';
 import DocumentView from './components/views/DocumentView';
-import SocialSummarizeView from './components/views/SocialSummarizeView';
+import SocialAnalyticsView from './components/views/SocialAnalyticsView';
 import VideoSummarizeView from './components/views/VideoSummarizeView';
 import MyEmailView from './components/views/MyEmailView';
 
